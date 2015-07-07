1403fdcabe3a0f60255d14112437a104e672b20b
wangzhong <wangzhong@babydeMacBook-Pro.local>
2015-07-07 14:51:45 +0800

    初始化工程

---
+++ PicTalk/PicTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PicTalk
+//
+//  Created by wangzhong on 15/7/7.
+//  Copyright (c) 2015年 wangzhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
